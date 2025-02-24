a2f8750db8e2f33921851ac5163c32a17da8d6a1
Khpehehko <10201khpehehko@gmail.com>
2025-02-24 13:32:32 -0500

    gdscript learning

--- breadknight/character_body_2d.gd
+++ breadknight/character_body_2d.gd
@@ -4,12 +4,15 @@ extends CharacterBody2D
 var attack_power: int = 3
 var damage: int = 2
 var text = "Attack:" + str(attack_power)
-
+var roll = randf()
 const GRAVITY = 9.81
 
 func _ready():
 	print(text)
-		
+	var result = add(10,21)
+	result = add(result,-5)
+	print(result)
+	
 func _input(event):
 	if event.is_action_pressed("Jump"):
 		health -= 2 
@@ -19,10 +22,19 @@ func _input(event):
 			health = 0
 			print("You died")
 		elif health < 6:
-			print("You are injured")
+			print("You are inju red")
 		else:
 			print("You are fine")
+	if roll <= 0.8:
+		print("New Skill Acquired")
+	else:
+		print("Change form")
+	
 			
 func respawn():
 	pass
+	
+func add(num1,num2):
+	var result = num1 + num2
+	return result
 	
